57ed000697059473b34c065c50062acf951d2c04
xiaoxue9402 <shirleyyz9402@gmail.com>
2021-02-14 22:33:22 -0500

    Keypad logic finalized

--- src/index.js
+++ src/index.js
@@ -4,60 +4,69 @@ const app = document.getElementById("app");
 const searchSwitch = document.getElementById("search-switch");
 const searchInput = document.getElementById("search-input");
 const deleteButton = document.getElementById("delete-button");
-const keypadNums = document.getElementsByClassName("keypad-num")
-function onLoad() {
+const keypadNums = document.getElementsByClassName("keypad-num");
+async function onLoad() {
   searchSwitch.addEventListener("change", onSwitch);
   searchInput.addEventListener("input", event => onInput(event));
   deleteButton.addEventListener("click", event => onInput(event));
-  keypadNums.forEach(key => key.addEventListener("click", event => onInput(event)))
+  let keys = [...keypadNums];
+  keys.forEach(key =>
+    key.addEventListener("click", event => onInput(event, key))
+  );
   console.log("loooaaaadddeed");
 }
 let state = {
-  searchBy: 'name',
+  searchBy: "name",
   keyLetters: {
-    "2": ['a', 'b', 'c'],
-    "3": ['d', 'e', 'f'],
-    "4": ['g', 'h', 'i'],
-    "5": ['j', 'k', 'l'],
-    "6": ['m', 'n', 'o'],
-    "7": ['p', 'q', 'r', 's'],
-    "8": ['t', 'u', 'v'],
-    "9": ['w', 'x', 'y', 'z']
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
   },
-  currentInput: '',
+  currentInput: "",
   currentOptions: []
-}
+};
 function onSwitch() {
-  searchSwitch.dataset.type === "name"
+  state.searchBy === "name"
     ? (searchSwitch.dataset.type = "number")
     : (searchSwitch.dataset.type = "name");
-    state.searchBy = searchSwitch.dataset.type
+  state.searchBy = searchSwitch.dataset.type;
   console.log(searchSwitch.dataset.type);
 }
 
-function onInput(event) {
-  let query;
-  let key = event.target.dataset.num
+function onInput(event, key) {
+  console.log(event.target.id);
 
-   if (event.type === "input") {
-    query = event.target.value;
-  } else if (event.type === "click") {
-    if (state.searchBy === "name") {
-      let letters = state.keyLetters[event.target.dataset.num]
-      if (state.currentOptions.length > 0) {
-        for (let i = 0; i < state.currentOptions.length; i++) {
-          for (let j = 0; j < letters.length; j++) {
-            state.currentOptions.push(state.currentOptions[i] + letters[j])
-          }
-        }
-      }
-    }
-  } else {
+  if (event.target.id === "delete-button") {
     if (searchInput.value.length > 0) {
       searchInput.value = searchInput.value.substr(
         0,
         searchInput.value.length - 1
       );
+    }
+  } else if (event.type === "click") {
+    let num = key.dataset.num;
+
+    if (state.searchBy === "name") {
+      let letters = state.keyLetters[num];
+      if (state.currentOptions[0]) {
+        let strings = [];
+        letters.map(letter =>
+          state.currentOptions.forEach(string => {
+            string += letter;
+            strings.push(string);
+          })
+        );
+        state.currentOptions = strings;
+      } else {
+        state.currentOptions = [...letters];
+      }
+      state.currentInput = state.currentOptions[0];
+      searchInput.value = state.currentInput;
     }
   }
 }
